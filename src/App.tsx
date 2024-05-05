67bd8e481eff023baf8e66e1139976c337e04897
Fernando Avila <fernandoavilajunior@gmail.com>
2024-05-05 12:31:53 -0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div style={{ width: '100%', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: '20px' }}>
+      <h1>Página em construção!</h1>
+      <h4>Logo terá uma página aqui :)</h4>
+    </div>
+  );
+}
+
+export default App;
