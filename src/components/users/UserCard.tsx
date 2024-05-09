201d8b7386f1e8c588608cd10e4c812519d9a3a5
Fernando Avila <fernandoavilajunior@gmail.com>
2024-05-09 01:54:49 -0300

    card ajustments

--- src/components/users/UserCard.tsx
+++ src/components/users/UserCard.tsx
@@ -10,13 +10,15 @@ export default function UserCard() {
                 </div>
                 <div className="user-info">
                     <h4>Fernando Ávila</h4>
-                    <h6>Desenvolvedor Full-Stack</h6>
+                    <h6>Full Stack Developer</h6>
                 </div>
                 <ul className="user-social">
-                    <li><a href="mailto:avila@ofernandoavila.com" target="_blank" rel="noreferrer"><i className="fa fa-envelope"></i></a></li>
                     <li><a href="https://instagram.com/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a></li>
                     <li><a href="https://www.youtube.com/@ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-youtube"></i></a></li>
                     <li><a href="https://www.github.com/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a></li>
+                    <li><a href="https://www.behance.com/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-behance"></i></a></li>
+                    <li><a href="mailto:avila@ofernandoavila.com" target="_blank" rel="noreferrer"><i className="fa fa-envelope"></i></a></li>
                 </ul>
             </div>
         </Card>
