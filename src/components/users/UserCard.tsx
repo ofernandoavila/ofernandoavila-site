fd1f08819715d5b8a9a1e5217504519d8bdedd56
Fernando Avila <fernandoavilajunior@gmail.com>
2024-05-09 02:29:42 -0300

    ajusts

--- src/components/users/UserCard.tsx
+++ src/components/users/UserCard.tsx
@@ -11,13 +11,10 @@ export default function UserCard() {
                 <div className="user-info">
                     <h4>Fernando Ávila</h4>
                     <h6>Full Stack Developer</h6>
-<<<<<<< HEAD
                     <section>
                         <span className="location"><i className="fa-solid fa-location-dot"></i>Rio de Janeiro, Brasil</span>
                         <span className="workplace"><i className="fa-solid fa-suitcase"></i>CCAA LTDA.</span>
                     </section>
-=======
->>>>>>> main
                 </div>
                 <ul className="user-social">
                     <li><a href="https://instagram.com/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a></li>
