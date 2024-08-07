746191114997a74bf1ee3ec4f6438ffc702033be
Fernando Avila <fernandoavilajunior@gmail.com>
2024-08-07 13:09:47 -0300

    Ajustes de responsividade

--- src/components/users/UserCard.tsx
+++ src/components/users/UserCard.tsx
@@ -3,10 +3,10 @@ import PublicImage from "../basic-ui/PublicImage";
 export default function UserCard() {
     return (
         <div className="user-card">
-            <div className="profile-image">
-                <PublicImage src="user_image.png" />
-            </div>
             <div className="user-info">
+                <div className="profile-image">
+                    <PublicImage src="user_image.png" />
+                </div>
                 <h4>Fernando Ávila</h4>
                 <h6>Full Stack Developer</h6>
                 {/* <section>
@@ -14,16 +14,18 @@ export default function UserCard() {
                         <span className="location"><i className="fa-solid fa-location-dot"></i>Rio de Janeiro, Brasil</span>
                     </section> */}
             </div>
-            <ul className="user-links">
-                <li><a href="mailto:avila@ofernandoavila.com" target="_blank" rel="noreferrer"><span>Enviar um e-mail</span></a></li>
-                <li><a href="https://www.github.com/ofernandoavila" target="_blank" rel="noreferrer"><span>Meus projetos</span></a></li>
-            </ul>
-            <ul className="user-social">
-                <li><a href="https://instagram.com/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a></li>
-                <li><a href="https://www.youtube.com/@ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-youtube"></i></a></li>
-                <li><a href="https://www.linkedin.com/in/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a></li>
-                <li><a href="https://www.github.com/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a></li>
-            </ul>
+            <div className="tree">
+                <ul className="user-links">
+                    <li><a href="mailto:avila@ofernandoavila.com" target="_blank" rel="noreferrer"><span>Enviar um e-mail</span></a></li>
+                    <li><a href="https://www.github.com/ofernandoavila" target="_blank" rel="noreferrer"><span>Meus projetos</span></a></li>
+                </ul>
+                <ul className="user-social">
+                    <li><a href="https://instagram.com/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a></li>
+                    <li><a href="https://www.youtube.com/@ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-youtube"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a></li>
+                    <li><a href="https://www.github.com/ofernandoavila" target="_blank" rel="noreferrer"><i className="fa-brands fa-github"></i></a></li>
+                </ul>
+            </div>
         </div>
     );
 }
